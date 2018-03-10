e52c2a7909a2be073a62b42be88b97f07136e93b
viher3 <viher_3@hotmail.com>
2018-03-10 13:53:28 +0100

    user component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { UserComponentComponent } from './user-component/user-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-component/user-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-component',
+  templateUrl: './user-component.component.html',
+  styleUrls: ['./user-component.component.css']
+})
+export class UserComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
