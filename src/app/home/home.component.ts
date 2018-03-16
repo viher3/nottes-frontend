cb3099e60d43924f58e8c91db3a967d3120c3028
viher3 <viher_3@hotmail.com>
2018-03-16 20:35:45 +0100

    Home Component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/user/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router){ }
+
+  ngOnInit() 
+  {
+  	// check if user is already signed in
+	if( this.authService.isLoggedIn() ) this.router.navigateByUrl('dashboard');
+  }
+
+}
