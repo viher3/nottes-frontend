d66e0781ca3a8b6d6db124c8ed1389b55fa467d8
viher3 <viher_3@hotmail.com>
2018-03-19 21:03:04 +0100

    Sidebar styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
