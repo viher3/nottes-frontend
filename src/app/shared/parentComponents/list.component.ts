5b0e7f395a9443250bbdb19bd14058d90605be45
viher3 <viher_3@hotmail.com>
2018-04-11 19:33:02 +0200

    ListComponent with select eitems methods

--- src/app/shared/parentComponents/list.component.ts
+++ src/app/shared/parentComponents/list.component.ts
@@ -9,8 +9,40 @@ export class ListComponent
 {
 	constructor(){ }
 
-	prueba()
+	public  selectedItems: number[] = [];
+  	public  selectedAll: boolean = false;
+  	public  listElements: JSON;
+
+	selectFromList(item)
 	{
-		alert("prueba ok");
+	    if( this.selectedItems.indexOf(item) > -1 ) 
+	    {
+	      // remove item
+	      var index = this.selectedItems.indexOf(item);
+	      this.selectedItems.splice(index, 1);
+	    }
+	    else
+	    {
+	      // add item
+	      this.selectedItems.push(item);
+	    }
+	}
+
+	selectAll(event)
+	{
+	    this.selectedItems = [];
+	    if( ! event.target.checked ) return;
+
+	    // add all items
+	    for(var i in this.listElements) 
+	    {
+	      this.selectedItems.push(this.listElements[i]);
+	    }
+	}
+
+	isSelected(item)
+	{
+	    if( this.selectedItems.indexOf(item) > -1 ) return true;
+	    return false;
 	}
 }
