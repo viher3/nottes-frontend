d6680c3aeed468f18b3c0a588d0174b81285dd9d
viher3 <viher_3@hotmail.com>
2018-04-30 15:50:20 +0200

    security config api request

--- src/app/configuration/general/generalConfiguration.component.ts
+++ src/app/configuration/general/generalConfiguration.component.ts
@@ -36,8 +36,7 @@ export class GeneralConfigurationComponent implements OnInit
 
   ];
 
-  private apiUrl:
-   string = AppConfig.settings.api.api_url;
+  private apiUrl : string = AppConfig.settings.api.api_url;
 
   ngOnInit()
   {
--- src/app/configuration/security/securityConfiguration.component.ts
+++ src/app/configuration/security/securityConfiguration.component.ts
@@ -5,6 +5,7 @@ import { ToastrService } from 'ngx-toastr';
 import { AuthHttp } from 'angular2-jwt';
 import { SpinnerComponent } from 'app/shared';
 import { TranslateService } from "@ngx-translate/core";
+import { AuthService } from 'app/user/auth.service';
 
 @Component({
   selector: 'security-configuration',
@@ -14,25 +15,78 @@ import { TranslateService } from "@ngx-translate/core";
 export class SecurityConfigurationComponent implements OnInit 
 {
   constructor(
-  	protected toastr: ToastrService,
-    protected authHttp: AuthHttp,
-    protected translator: TranslateService,
+  	protected toastr : ToastrService,
+    protected authHttp : AuthHttp,
+    protected translator : TranslateService,
+    private auth : AuthService
   ) 
   {
     
   }
 
+  public loading : boolean = false;
   public newPassword : string = "";
   public newPassword2 : string = "";
   public currentPassword : string = "";
+  private apiUrl : string = AppConfig.settings.api.api_url;
 
-  ngOnInit()
+  ngOnInit(){ }
+
+  formIsValid()
   {
-    	
+    if( ! this.newPassword.length || ! this.newPassword2.length )
+    {
+      return "required_fields";
+    }
+    else if( 
+      (this.newPassword != this.newPassword2) &&
+      this.newPassword.length &&
+      this.newPassword2.length
+    )
+    {
+      return "passwords_dont_match";
+    }
+    
+    return true;
   }
 
-  saveChanges()
+  saveChanges() : void
   {
-    
+    this.loading = true;
+
+    this.authHttp.put(this.apiUrl + "/configuration/security/password", 
+    {
+      "currentPassword" : this.currentPassword,
+      "newPassword" : this.newPassword2
+    })
+    .subscribe(
+
+      data => {
+
+        this.translator.get('common.changes_saved_success_mssg').subscribe( (translation: string) => {
+          this.toastr.success(translation);
+        });
+
+        this.loading = false;
+      },
+
+      error => {
+
+        this.auth.checkJwtHasExpiredInServerRequest(error);
+
+        let jsonError = JSON.parse(error._body);
+
+        if(jsonError.error == "invalid_password")
+        {
+          this.translator.get('components.configuration.wrong_password_mssg')
+          .subscribe( (translation: string) => {
+            this.toastr.error(translation);
+          });
+        }
+
+        this.loading = false;
+      }
+
+    ); 
   }
 }
