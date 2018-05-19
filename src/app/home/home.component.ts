de491966654408fd4d8d0ee9689826aff41dac33
viher3 <viher_3@hotmail.com>
2018-05-19 23:32:51 +0200

    (Fix) Icon scroll to top

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,10 +27,7 @@ export class HomeComponent implements OnInit {
 
   scrollTop()
   {
-    $(".scrollTop").on("click", function()
-    {
-      $("html, body").animate({ scrollTop: 0 }, 600);
-    });
+    $("html, body").animate({ scrollTop: 0 }, 500);
   }
 
 }
