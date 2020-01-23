5a42750f88e4fb1835b1faafa0a5ca0af91d76fa
viher3 <viher_3@hotmail.com>
2020-01-23 21:51:11 +0100

    Added redux dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import ReactNotification from 'react-notifications-component'
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <Provider>
+        <ReactNotification />
+        <App />
+    </Provider>
+</BrowserRouter>,
+document.getElementById('wrapper')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
