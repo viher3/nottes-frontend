a52a695688f9903af65d0b573e84e5ec1ec2ac1f
viher3 <viher_3@hotmail.com>
2020-05-10 19:19:08 +0200

    Created ShowDocument view

--- src/Components/Notte/NotteList.js
+++ src/Components/Notte/NotteList.js
@@ -15,17 +15,16 @@ class NotteList extends React.Component {
             <div className="card item" onDoubleClick={() => this.props.history.push(RoutesPath.showDocument + this.props.item.id) }>
                 <ContextMenuTrigger id="notteListItemMenu" collect={() => this.props.item}>
                     <div className="card-block">
-                        <h4 className="card-title mt-3">
+                        <h4 className="card-title">
                             <NotteName item={this.props.item}/>
                         </h4>
                         <div className="meta">
                             {this.props.item.tags}
                         </div>
-                        <div className="card-text">...</div>
                     </div>
                     <div className="card-footer">
                         <small>
-                            Last updated <Moment fromNow>{this.props.item.updated_at}</Moment>
+                            {this.props.item.creator_user.username} - Last updated <Moment fromNow>{this.props.item.updated_at}</Moment>
                         </small>
                     </div>
                 </ContextMenuTrigger>
