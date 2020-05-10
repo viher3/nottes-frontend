10e6121416721449a9f78f54e14284a15828715d
viher3 <viher_3@hotmail.com>
2020-05-10 15:29:45 +0200

    Changes BrowserRouter to HashRouter

--- src/Views/Notte/ShowDocument.js
+++ src/Views/Notte/ShowDocument.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Card} from "reactstrap";
+import Container from 'Views/Container';
 
 class ShowDocument extends React.Component {
 
     render() {
         return (
-            <span>TODO</span>
+            <Container>
+                <h1>aaa</h1>
+            </Container>
         );
     }
 
